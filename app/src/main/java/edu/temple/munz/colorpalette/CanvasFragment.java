60c81d7f399c66c52fc22903cf7977f3181c523c
ElizabethMunz <munz@temple.edu>
2018-10-10 12:09:44 -0400

    now selecting from dropdown creates a fragment of the color on the same activity, instead of starting a new activity.

--- app/src/main/java/edu/temple/munz/colorpalette/CanvasFragment.java
+++ app/src/main/java/edu/temple/munz/colorpalette/CanvasFragment.java
@@ -1,29 +1,99 @@
 package edu.temple.munz.colorpalette;
 
-
+import android.content.Context;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 
 /**
  * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link CanvasFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link CanvasFragment#newInstance} factory method to
+ * create an instance of this fragment.
  */
 public class CanvasFragment extends Fragment {
 
+    private static final String BG_COLOR = "bgcolor";
+    private String bgColor;
+
+
+    private OnFragmentInteractionListener mListener;
 
     public CanvasFragment() {
         // Required empty public constructor
     }
 
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param color A string representing the color to be used as the background
+     * @return A new instance of fragment CanvasFragment.
+     */
+    public static CanvasFragment newInstance(String color) {
+        CanvasFragment fragment = new CanvasFragment();
+        Bundle args = new Bundle();
+        args.putString(BG_COLOR, color);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            bgColor = getArguments().getString(BG_COLOR);
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // set background of the fragment's FrameLayout to whatever color was selected
+        container.setBackgroundColor(Color.parseColor(bgColor));
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_canvas, container, false);
     }
 
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
 }
--- app/src/main/java/edu/temple/munz/colorpalette/PaletteActivity.java
+++ app/src/main/java/edu/temple/munz/colorpalette/PaletteActivity.java
@@ -1,5 +1,11 @@
 package edu.temple.munz.colorpalette;
 
+// I had to manually change these imports because it was using android.app.Fragment,
+// which is deprecated and was causing mismatch errors
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Color;
@@ -46,9 +52,22 @@ public class PaletteActivity extends AppCompatActivity {
                 //only open new view if the user has selected something other than the default/first element
                 if(!adapterView.getItemAtPosition(i).equals("")) {
                     //layout.setBackgroundColor(Color.parseColor(colors[i]));
-                    Intent startCanvasIntent = new Intent(PaletteActivity.this, CanvasActivity.class);
-                    startCanvasIntent.putExtra("bgColor", colorIDs[i]);
-                    startActivityForResult(startCanvasIntent, 117);
+                    //below 3 lines are to start a new activity; we are changing that to showing a new fragment
+                    //Intent startCanvasIntent = new Intent(PaletteActivity.this, CanvasActivity.class);
+                    //startCanvasIntent.putExtra("bgColor", colorIDs[i]);
+                    //startActivityForResult(startCanvasIntent, 117);
+
+
+                    CanvasFragment canvasFragment = new CanvasFragment();
+                    canvasFragment = canvasFragment.newInstance(colorIDs[i]);
+
+                    //create the fragment
+                    FragmentManager fm = getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fm.beginTransaction();
+                    fragmentTransaction.replace(R.id.fragmentFrame, canvasFragment).addToBackStack(null).commit();
+
+
+
                 }
             }
 
