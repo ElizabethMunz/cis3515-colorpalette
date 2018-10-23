81786414e406fcbd303db30fe401e88e6eba42c2
ElizabethMunz <munz@temple.edu>
2018-10-23 15:27:19 -0400

    created files for different layouts & two separate fragments

---
+++ app/src/main/java/edu/temple/munz/colorpalette/PickerFragment.java
@@ -0,0 +1,29 @@
+package edu.temple.munz.colorpalette;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * Contains the spinner with color choices
+ */
+public class PickerFragment extends Fragment {
+
+
+    public PickerFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_picker, container, false);
+    }
+
+}
